2dc11d855ed135b557540d70d20f664a2267487c
king8879 <k4507244@gmail.com>
2025-05-01 11:49:32 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,5 +5,5 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22923037"))
 API_HASH = environ.get("API_HASH", "dfb3666878b3851460a58461c5a50f5b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8193339718:AAHa0oIw2eeBX6tXiUPb0Tt3I2X7a0bW2jE")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8199258519:AAHCtP38ev-1Za_KHCQBiRMDs1J9r-t4B-E")
 AUTH_USERS = "6554343173"
